584234bd23a3ed54b1ff20725110c8478b9a1e7f
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-01 15:50:11 -0700

    Added test script into the project and user stories 1 and 2
    test 1 and 2 passed

--- markdown-previewer/src/App.tsx
+++ markdown-previewer/src/App.tsx
@@ -5,8 +5,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <div>
+      <textarea id="editor">
+
+      </textarea>
+      <div id="preview">
+        
+      </div>
+    </div>
   )
 }
 
